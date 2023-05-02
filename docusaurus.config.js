eb5180eba7acd1e2f7868e4751aa14fed1f9e81c
Senol <senols@gmail.com>
2023-05-02 13:24:43 +0300

    ok

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://roaring-douhua-296fcf.netlify.app/',
+  url: 'https://docs.aipower.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
