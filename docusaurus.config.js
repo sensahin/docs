12192f07d36a297e0a359b0e75379d85355f0f90
Senol <senols@gmail.com>
2023-05-02 12:52:47 +0300

    ok

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,19 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sensahin.github.io',
+  url: 'https://roaring-douhua-296fcf.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'sensahin', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
